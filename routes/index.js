53c9a60fc5f98aa3295f0e0ba73eb53e0e11e5d0
Raphael Kim <sudokky.dev@gmail.com>
2015-06-05 15:19:11 +0000

    Send HTTP request message to Arduino via serial port
    - REST : http://rpi:port/arduino/display?message=ANY_MESSAGE

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var arduino = require('../lib/arduino');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Findmon - 2015 SBA IoT Hackathon' });
+    res.render('index', {
+        title: 'Findmon - 2015 SBA IoT Hackathon'
+    });
 });
 
+router.get('/arduino/display', function(req, res, next) {
+	var message = req.param("message");
+
+	arduino.sendMessage(message, function(err, results) {
+		res.render('arduinoRes', {
+			title: 'Arduino Response',
+			error: err,
+			results: results
+		});
+	});
+
+});
 module.exports = router;
