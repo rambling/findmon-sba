62adb322661ea91828e1837b963d63a0745fd254
raphael <sudokky.dev@gmail.com>
2015-06-05 22:16:25 +0900

    change the app and repo name

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Findmon - 2015 SBA IoT Hackathon' });
 });
 
 module.exports = router;
